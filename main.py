3b348504ff1406cb3fb595678400c953fe77641d
shinkai-tester <alinlaegnor@gmail.com>
2021-09-18 00:23:21 +0300

    по-прежнему пробуем парсить json, ловим JSONDecodeError

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from json.decoder import JSONDecodeError
 import requests
 
-payload = {"name": "Sasha"}
-response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
+response = requests.get("https://playground.learnqa.ru/api/hello", params={"name": "Sasha"})
 print(response.text)
+
+try:
+    parsed_response_text = response.json()
+    print(parsed_response_text["answer"])
+except JSONDecodeError:
+    print("Response is not a JSON format")
+
