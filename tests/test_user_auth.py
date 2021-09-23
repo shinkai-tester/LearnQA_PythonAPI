b43465ea23b242b84f9d14e4d68882a775a1a4e4
shinkai-tester <alinlaegnor@gmail.com>
2021-09-24 01:08:11 +0300

    added class Assertions for assertions reuse

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -1,7 +1,7 @@
 import pytest
 import requests
 from lib.base_case import BaseCase
-
+from lib.assertions import Assertions
 
 class TestUserAuth(BaseCase):
     exclude_params = [
@@ -25,13 +25,15 @@ class TestUserAuth(BaseCase):
         response2 = requests.get(
             "https://playground.learnqa.ru/api/user/auth",
             headers={"x-csrf-token": self.token},
-            cookies={"auth_sid": self.auth_sid})
+            cookies={"auth_sid": self.auth_sid}
+        )
 
-        assert "user_id" in response2.json(), "There is no user id in the 2nd response"
-        user_id_check_method = response2.json()["user_id"]
-
-        assert self.user_id_auth_method == user_id_check_method, "User id from auth method is not equal to " \
-                                                                 "user id from check method"
+        Assertions.assert_json_value_by_name(
+            response2,
+            "user_id",
+            self.user_id_auth_method,
+            "User id from auth method is not equal to user id from check method"
+        )
 
     @pytest.mark.parametrize('condition', exclude_params)
     def test_negative_auth_check(self, condition):
@@ -45,7 +47,10 @@ class TestUserAuth(BaseCase):
                 "https://playground.learnqa.ru/api/user/auth",
                 cookies={"auth_sid": self.auth_sid})
 
-        assert "user_id" in response2.json(), "There is no user id in the second response"
+        Assertions.assert_json_value_by_name(
+            response2,
+            "user_id",
+            0,
+            f"User is authorized with condition {condition}"
+        )
 
-        user_id_from_check_method = response2.json()["user_id"]
-        assert user_id_from_check_method == 0, f"User is authorized with condition {condition}"
