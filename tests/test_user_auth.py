e72af7bac9b3b817c190551c08daa77b203a9808
shinkai-tester <alinlaegnor@gmail.com>
2021-09-24 00:51:18 +0300

    created class BaseCase for the code that is reused

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -1,7 +1,9 @@
 import pytest
 import requests
+from lib.base_case import BaseCase
 
-class TestUserAuth:
+
+class TestUserAuth(BaseCase):
     exclude_params = [
         "no_cookie",
         "no_token"
@@ -15,32 +17,29 @@ class TestUserAuth:
 
         response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
 
-        assert "auth_sid" in response1.cookies, "There is no auth cookie in the response"
-        assert "x-csrf-token" in response1.headers, "There is no CSRF token header in the response"
-        assert "user_id" in response1.json(), "There is no user id in the response"
-
-        self.auth_sid = response1.cookies.get("auth_sid")
-        self.token = response1.headers.get("x-csrf-token")
-        self.user_id_auth_method = response1.json()["user_id"]
+        self.auth_sid = self.get_cookie(response1, "auth_sid")
+        self.token = self.get_header(response1, "x-csrf-token")
+        self.user_id_auth_method = self.get_json_value(response1, "user_id")
 
     def test_auth_user(self):
         response2 = requests.get(
             "https://playground.learnqa.ru/api/user/auth",
             headers={"x-csrf-token": self.token},
-            cookies={"auth_sid" : self.auth_sid})
+            cookies={"auth_sid": self.auth_sid})
 
         assert "user_id" in response2.json(), "There is no user id in the 2nd response"
         user_id_check_method = response2.json()["user_id"]
 
-        assert self.user_id_auth_method == user_id_check_method, "User id from auth method is not equal to user id from check method"
+        assert self.user_id_auth_method == user_id_check_method, "User id from auth method is not equal to " \
+                                                                 "user id from check method"
 
     @pytest.mark.parametrize('condition', exclude_params)
     def test_negative_auth_check(self, condition):
 
         if condition == "no_cookie":
             response2 = requests.get(
-            "https://playground.learnqa.ru/api/user/auth",
-            headers={"x-csrf-token": self.token})
+                "https://playground.learnqa.ru/api/user/auth",
+                headers={"x-csrf-token": self.token})
         else:
             response2 = requests.get(
                 "https://playground.learnqa.ru/api/user/auth",
