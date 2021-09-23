51abb59f8ad739ddfe43bb0b8568ba6ca7b273ae
shinkai-tester <alinlaegnor@gmail.com>
2021-09-23 23:42:45 +0300

    test_first_api is parameterized

--- first_steps/test_first_api.py
+++ first_steps/test_first_api.py
@@ -1,9 +1,16 @@
+import pytest
 import requests
 
 class TestFirstApi:
-    def test_hello_call(self):
+    names = [
+        ("Sasha"),
+        ("Dmitriy"),
+        ("")
+    ]
+
+    @pytest.mark.parametrize('name', names)
+    def test_hello_call(self, name):
         url = "https://playground.learnqa.ru/api/hello"
-        name = 'Sasha'
         data = {'name': name}
 
         response = requests.get(url, params=data)
@@ -13,6 +20,10 @@ class TestFirstApi:
         response_dict = response.json()
         assert "answer" in response_dict, "There is no field 'answer' in the response"
 
-        expected_response_text = f"Hello, {name}"
+        if len(name) == 0:
+            expected_response_text = "Hello, someone"
+        else:
+            expected_response_text = f"Hello, {name}"
+
         actual_response_text = response_dict["answer"]
         assert actual_response_text == expected_response_text, "Actual text in the response is not correct"
