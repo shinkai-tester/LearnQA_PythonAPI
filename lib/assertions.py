ed87238e76efa9494d23d23b2f5a2b1fa21ffbef
Aleksandra Klimantova <alinlaegnor@gmail.com>
2021-10-01 04:17:35 +0300

    added test_create_user_successfully

--- lib/assertions.py
+++ lib/assertions.py
@@ -12,3 +12,17 @@ class Assertions:
 
         assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
         assert response_as_dict[name] == expected_value, error_message
+
+    @staticmethod
+    def assert_json_has_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+
+        assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+
+    @staticmethod
+    def assert_status_code(response: Response, expected_code):
+        assert response.status_code == expected_code, \
+            f"Unexpected status code! Expected: {expected_code}. Actual: {response.status_code}"
--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,9 +1,36 @@
 import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+from datetime import datetime
+from faker import Faker
 
 
 class TestUserRegister(BaseCase):
+    def setup(self):
+        fake = Faker()
+        base_part = "learnqa"
+        domain = "example.com"
+        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+        self.email = f"{base_part}{random_part}@{domain}"
+        self.firstName = fake.first_name()
+        self.lastName = fake.last_name()
+        self.username = fake.user_name()
+
+    def test_create_user_successfully(self):
+        data = {
+            'password': '123',
+            'username': self.username,
+            'firstName': self.firstName,
+            'lastName': self.lastName,
+            'email': self.email
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        Assertions.assert_status_code(response, 200)
+        Assertions.assert_json_has_key(response, "id")
+        print(self.email)
+
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
         data = {
@@ -16,6 +43,6 @@ class TestUserRegister(BaseCase):
 
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
 
-        assert response.status_code == 400, f"Unexpected status code {response.status_code}"
+        Assertions.assert_status_code(response, 400)
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", \
             f'Unexpected response content {response.content} '
